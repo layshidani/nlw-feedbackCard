fdc4d33f11c9bd5b7057ddea19579df108713db8
Lays Hidani <lays.hidani@gmail.com>
2022-05-03 14:14:00 -0300

    feat: implements FeedbackContentStep

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import bug from "../../assets/bug.svg";
 import idea from "../../assets/idea.svg";
 import thought from "../../assets/thought.svg";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
 import { FeedBackTypesStep } from "./Steps/FeedbackTypesStep";
 
 export const feedbackTypes = {
@@ -34,16 +35,23 @@ export type FeedbackType = keyof typeof feedbackTypes;
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
 
+  function handleRestartFeedback() {
+    setFeedbackType(null);
+  }
+
   return (
-    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col items-center shadow-lg w-[calc(100vh-2rem)] md:w-auto">
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vh-2rem)] md:w-auto">
       {!feedbackType ? (
         <FeedBackTypesStep onFeedbackTypeClicked={setFeedbackType} />
       ) : (
-        <p>Hello</p>
+        <FeedbackContentStep
+          feedbackType={feedbackType}
+          onRestartFeedbackRequested={handleRestartFeedback}
+        />
       )}
 
       <footer className="text-xs text-neutral-400">
-        Feito com ♥ pela
+        Feito com ♥ pela{' '}
         <a
           className="underline underline-offset-2"
           href="https://rocketseat.com.br"
