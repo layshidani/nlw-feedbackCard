febcd860593d9ca304f9ef1b1163add5f44bf87c
Lays Hidani <lays.hidani@gmail.com>
2022-05-02 21:27:55 -0300

    chore: adds phosphore react

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,8 @@
-import { useState } from "react";
+import { Widget } from "./Widget";
 
-interface ButtonProps {
-  text?: string;
-}
-
-function Button(props?: ButtonProps) {
+export function App() {
   return (
-    <button className="bg-violet-200 hover:bg-violet-400 transition-colors px-4 m-4 h-10 rounded">
-      {props?.text ?? "Meu botão"}
-    </button>
-  );
-}
-
-function App() {
-  return (
-    <div className="flex gap-2">
-      <h1>Helloo</h1>
-      <Button text="Confirmar"></Button>
-      <Button></Button>
-    </div>
+    <Widget></Widget>
   );
 }
 
---
+++ src/Widget.tsx
@@ -0,0 +1,7 @@
+import { ChatTeardropDots } from "phosphor-react";
+
+export function Widget() {
+  return (
+    <ChatTeardropDots className="w-6 h-6" />
+  );
+}
