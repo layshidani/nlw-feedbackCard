1fb6bce355169d3980a944be6ce5a47bd5c0a022
Lays Hidani <lays.hidani@gmail.com>
2022-05-03 13:44:58 -0300

    refact: implements FeedBackTypesStep

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
-import { CloseButton } from "../CloseButton";
 import bug from "../../assets/bug.svg";
 import idea from "../../assets/idea.svg";
 import thought from "../../assets/thought.svg";
+import { FeedBackTypesStep } from "./Steps/FeedbackTypesStep";
 
-const feedbackTypes = {
+export const feedbackTypes = {
   BUG: {
     title: " Problema",
     image: {
@@ -29,33 +29,18 @@ const feedbackTypes = {
   },
 };
 
-type FeedbackType = keyof typeof feedbackTypes;
+export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
 
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col items-center shadow-lg w-[calc(100vh-2rem)] md:w-auto">
-      <header>
-        <span className="text-xl leading-6">Deixe seu feedback</span>
-        <CloseButton />
-      </header>
-
-      <div className="flex py-8 gap-2 w-full">
-        {Object.entries(feedbackTypes).map(([key, value]) => {
-          return (
-            <button
-              key={key}
-              onClick={() => setFeedbackType(key as FeedbackType)}
-              className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-              type="button"
-            >
-              <img src={value?.image?.source} alt={value?.image?.alt} />
-              <span>{value?.title}</span>
-            </button>
-          );
-        })}
-      </div>
+      {!feedbackType ? (
+        <FeedBackTypesStep onFeedbackTypeClicked={setFeedbackType} />
+      ) : (
+        <p>Hello</p>
+      )}
 
       <footer className="text-xs text-neutral-400">
         Feito com ♥ pela
