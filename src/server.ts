0b3e532c6a4e1b393f4652f625ba5172531f960a
Lays Hidani <lays.hidani@gmail.com>
2022-05-04 16:20:40 -0300

    feat: configs prisma and creates feedbacks route

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,23 @@
+import { prisma } from "./prisma";
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
-app.get("/users", (req, res) => {
-  return res.send("Hello World");
+app.post("/feedbacks", async (req, res) => {  
+  const {type, comment, screenshot} = req?.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+
+  return res.status(201).json({ data: feedback });
 });
 
 app.listen(3333, () => {
-  console.log("-> server running...");
+  console.log("-> HTTP server running...");
 });
