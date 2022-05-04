a01b667314986bf3c9bde7e4809f843e4c59d57f
Lays Hidani <lays.hidani@gmail.com>
2022-05-03 21:26:19 -0300

    feat: creates ScreenShotButton

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,8 @@
 import { ArrowLeft } from "phosphor-react";
+
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
+import { ScreenShotButton } from "./ScreenShotButton";
 
 interface FeedbackContentStepProps {
   feedbackType: FeedbackType;
@@ -42,6 +44,8 @@ export function FeedbackContentStep({
         ></textarea>
 
         <footer className="flex gap-2 mt-2">
+          <ScreenShotButton />
+
           <button
             type="submit"
             className="p-2 bg-brand-500 rounded-md border-transparent flex flex-1 justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
