509a99f0ecc29389963ce2822858ee14cfc49cca
Lays Hidani <lays.hidani@gmail.com>
2022-05-05 20:25:52 -0300

    feat: config CORS

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,15 @@
 import express from "express";
+import cors from "cors";
 
 import { routes } from "./routes";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "https://nlw-feedback-card.vercel.app/",
+  })
+);
 app.use(express.json());
 app.use(routes);
 
